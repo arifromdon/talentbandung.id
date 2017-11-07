29fe4a8c682f7dfcc766169d731cabfc72bfc432
Arif Romdon <arifdop182@gmail.com>
2017-11-07 19:21:30 +0700

    java script alert use addEventListener

---
+++ sesi4/assets/js/jsperdana.js
@@ -0,0 +1,8 @@
+
+function butt(event){
+	console.log(event.target.textContent);
+}
+document.getElementsByClassName('button-place');
+addEventListener("click", function(event) {
+    alert( event.target.textContent );
+  });
